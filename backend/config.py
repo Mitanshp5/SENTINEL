8a6a2ae359c20f63ae66fa5358655ede2d1ed3f7
Mitansh Prajapati <mitanshp2112@gmail.com>
2026-03-28 22:16:17 +0530

    chore: enable wildcard CORS for initial deployment

--- backend/config.py
+++ backend/config.py
@@ -54,7 +54,8 @@ class Settings(BaseSettings):
     cors_origins: list[str] = [
         "http://localhost:5173", 
         "http://localhost:3000",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "*"
     ]
 
     class Config:
